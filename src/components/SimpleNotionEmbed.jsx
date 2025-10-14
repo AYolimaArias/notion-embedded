42cb616a156072feaf488b91619e73c078c4208d
Angelica <angelica.arias33@gmail.com>
2025-10-14 14:18:54 -0500

    refactor: updating the iframe

--- src/components/SimpleNotionEmbed.jsx
+++ src/components/SimpleNotionEmbed.jsx
@@ -4,7 +4,7 @@ const SimpleNotionEmbed = () => {
   return (
     <div className="simple-notion-container">
       <iframe
-        src="https://v2-embednotion.com/28be9e2e2a388084842ff409d5ee27fd"
+        src="https://v2-embednotion.com/28ce9e2e2a38818a8300e190a6147cb4"
         className="notion-iframe"
         title="Notion Content"
         loading="lazy"
